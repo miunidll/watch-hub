a51d589c533b93456ca6f2c4bae2a99ea0361f03
miunidll <danieledits360@gmail.com>
2025-11-24 15:35:07 +0200

    Updated firebase config

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 
 // Firebase configuration - users should replace with their own config
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDVEDyaRPd_IfiSpvAfhKyV2eskcyv8nCA",
+  authDomain: "miunet-80973.firebaseapp.com",
+  projectId: "miunet-80973",
+  storageBucket: "miunet-80973.firebasestorage.app",
+  messagingSenderId: "371262973976",
+  appId: "1:371262973976:web:545db8f5b80fc55f9afb6d",
+  measurementId: "G-EVN6FGSK8P"
 };
 
 // Initialize Firebase
