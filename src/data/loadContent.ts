93fed6836c6cc8f1b9385f899d8133d457aa0079
miunidll <danieledits360@gmail.com>
2025-11-24 14:35:10 +0200

    Added stuff

---
+++ src/data/loadContent.ts
@@ -0,0 +1,13 @@
+import type { Content, Movie, TVShow } from "@/data/content";
+
+// Load all JSON files from movies and tv folders
+export function loadContent(): Content[] {
+  // Vite magic: eager imports all JSON files in the folder
+  const movieFiles = import.meta.glob("/src/content/movies/*.json", { eager: true });
+  const tvFiles = import.meta.glob("/src/content/tv/*.json", { eager: true });
+
+  const movies: Movie[] = Object.values(movieFiles) as Movie[];
+  const tvShows: TVShow[] = Object.values(tvFiles) as TVShow[];
+
+  return [...movies, ...tvShows];
+}
